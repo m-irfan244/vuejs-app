2bc6da436840b6bc0d7c215b2632ac6dbf2fc5cc
m-irfan244 <irfan.baitham123@gmail.com>
2021-04-30 21:21:53 +0500

    Vue app

---
+++ src/components/Button.vue
@@ -0,0 +1,13 @@
+<template>
+    <button :style="{ background: color }" class="btn"> {{text}} </button>
+</template>
+
+<script>
+export default {
+    name: 'Button', 
+    props: {
+        text: String,
+        color: String,
+    }
+}
+</script>
